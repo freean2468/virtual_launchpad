b0a8cbaa271345a35489d64c70008f36d4e0dfb8
sensebecommon <freean2468@gmail.com>
2020-12-21 02:37:05 +0900

    v0.3 - 한 미디 파일 정상 작동 가능한 수준
    앞으로 범용성을 위해 더 개발이 필요

--- src/application/MusicPlayer.java
+++ src/application/MusicPlayer.java
@@ -1,47 +1,162 @@
 package application;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 import javax.sound.midi.*;
 
 public class MusicPlayer implements ControllerEventListener{
-	RootController rc;
+	public static final int NOTE_ON_EVENT = 126;
+	public static final int NOTE_OFF_EVENT = 127;
+	public static final int NOTE_ON = 0x90;
+	public static final int NOTE_OFF = 0x80;
+	public static final String[] NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+	public static final int OCTAVE = 12;
+	
+	private RootController rc;
 	
 	public MusicPlayer(RootController _rc) {
 		rc = _rc;
 	}
 	
 	public void go() {
+		File myFile = null;
+		TreeMap<Integer, String> keyRange = null;
+		
 		try {
 			Sequencer sequencer = MidiSystem.getSequencer();
 			sequencer.open();
 			
-			int[] eventsIWant = {127};
+			int[] eventsIWant = {126, 127};
 			sequencer.addControllerEventListener(this, eventsIWant);
 			
-			Sequence seq = new Sequence(Sequence.PPQ, 4);
-			Track track = seq.createTrack();
+			// making a sequence and a track directly
+//			Sequence seq = new Sequence(Sequence.PPQ, 4);
+//			Track track = seq.createTrack();
+//			
+//			int r = 0;
+//			
+//			for (int i = 5; i < 60; i+= 4) {
+//				r = (int)((Math.random()*50)+1);
+//				track.add(makeEvent(144, 1, r, 100, i));
+//				track.add(makeEvent(176, 1, 127, r, i));	// register controller event
+//				track.add(makeEvent(128, 1, r, 100, i+2));
+//			}
+//			
+//			sequencer.setSequence(seq);
+//			sequencer.setTempoInBPM(220);
+//			sequencer.start();
 			
-			int r = 0;
+			myFile = new File("./mid/gymnopedie_1.mid");
+			Sequence seq = MidiSystem.getSequence(myFile);
+			Track trackForController = seq.createTrack();
 			
-			for (int i = 5; i < 60; i+= 4) {
-				r = (int)((Math.random()*50)+1);
-				track.add(makeEvent(144, 1, r, 100, i));
-				track.add(makeEvent(176, 1, 127, r, i));
-				track.add(makeEvent(128, 1, r, 100, i+2));
+			HashMap<Integer, String> map = new HashMap<Integer, String>();
+	        KeyComparator bvc = new KeyComparator(map);
+	        keyRange = new TreeMap<Integer, String>(bvc);
+			
+			int trackNumber = 0;
+			
+			// find out a key range.
+			for (Track track : seq.getTracks()) {
+				trackNumber++;
+				if (trackNumber == 2) {
+					for (int i = 0; i < track.size(); i++) {
+						MidiEvent event = track.get(i);
+						MidiMessage message = event.getMessage();
+						
+						if (message instanceof ShortMessage) {
+							ShortMessage sm = (ShortMessage) message;
+							if (sm.getCommand() == NOTE_ON) {
+								int key = sm.getData1();
+								int note = key % OCTAVE;
+								String noteName = NOTE_NAMES[note];
+								if (!map.containsKey(key)) {
+									map.put(key, noteName);
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			keyRange.putAll(map);
+			System.out.println("총 키 갯수 : " + keyRange.size());
+			System.out.println(keyRange);
+			
+			trackNumber = 0;
+			
+			for (Track track : seq.getTracks()) {
+				trackNumber++;
+				System.out.println("Track " + trackNumber + ": size = " + track.size());
+				System.out.println();
+				
+				if (trackNumber == 2) {
+					for (int i = 0; i < track.size(); i++) {
+						MidiEvent event = track.get(i);
+						long tick = event.getTick();
+						System.out.print("@" + tick + " ");
+						MidiMessage message = event.getMessage();
+						
+						if (message instanceof ShortMessage) {
+							ShortMessage sm = (ShortMessage) message;
+							int channel = sm.getChannel();
+							System.out.print("Channel: " + channel + " ");
+							if (sm.getCommand() == NOTE_ON) {
+								// bests : 9, 14, 114, 121
+								track.add(makeEvent(ShortMessage.PROGRAM_CHANGE, 0, 9, 0, tick));
+								int key = sm.getData1();
+								int octave = (key / OCTAVE) - 1;
+								int note = key % OCTAVE;
+								String noteName = NOTE_NAMES[note];
+								int velocity = sm.getData2();
+								System.out.println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity);
+								
+								trackForController.add(makeEvent(176, channel, NOTE_ON_EVENT, key-keyRange.firstKey(), tick));
+								trackForController.add(makeEvent(176, channel, NOTE_OFF_EVENT, key-keyRange.firstKey(), tick+(velocity*100)));
+							} else if (sm.getCommand() == NOTE_OFF) {
+								int key = sm.getData1();
+								int octave = (key / 12) - 1;
+								int note = key % 12;
+								String noteName = NOTE_NAMES[note];
+								int velocity = sm.getData2();
+								System.out.println("Note off, " + noteName + octave + " key=" + key + " velocity: " + velocity);
+							} else {
+								System.out.println("command: " + sm.getCommand());
+							}
+						} else {
+							System.out.println("Other message: " + message.getClass());
+						}
+					}
+				}
+				
+				System.out.println();
 			}
 			
 			sequencer.setSequence(seq);
-			sequencer.setTempoInBPM(220);
 			sequencer.start();
-		} catch (Exception ex) { ex.printStackTrace(); }
+		} catch (Exception ex) { 
+			ex.printStackTrace(); 
+		}
 	}
 
 	@Override
 	public void controlChange(ShortMessage event) {
-		System.out.println("la ~ data2 : " + event.getData2());
-		rc.handleBtn11Action();
+//		System.out.println("data1 : " + event.getData1() + " data2 : " + event.getData2());
+		if (event.getData1() == NOTE_ON_EVENT)
+			rc.launchpad.get(event.getData2()).setFill(Color.CORAL);
+		else if (event.getData1() == NOTE_OFF_EVENT)
+			rc.launchpad.get(event.getData2()).setFill(Color.WHITE);
 	}
 	
-	public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+	public MidiEvent makeEvent(int comd, int chan, int one, int two, long tick) {
 		MidiEvent event = null;
 		try {
 			ShortMessage a = new ShortMessage();
@@ -63,5 +178,23 @@ public class MusicPlayer implements ControllerEventListener{
 			event = new MidiEvent(a, tick);
 		} catch (Exception e) { }
 		return event;
+	}
+	
+	class KeyComparator implements Comparator<Integer> {
+	    Map<Integer, String> base;
+
+	    public KeyComparator(Map<Integer, String> base) {
+	        this.base = base;
+	    }
+
+	    // Note: this comparator imposes orderings that are inconsistent with
+	    // equals.
+	    public int compare(Integer a, Integer b) {
+	        if (a <= b) {
+	            return -1;
+	        } else {
+	            return 1;
+	        } // returning 0 would merge keys
+	    }
 	}
 }
--- src/application/RootController.java
+++ src/application/RootController.java
@@ -1,15 +1,26 @@
 package application;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.TreeMap;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class RootController implements Initializable {
+	@FXML private Rectangle btn1;
+	@FXML private Rectangle btn2;
+	@FXML private Rectangle btn3;
+	@FXML private Rectangle btn4;
+	@FXML private Rectangle btn5;
+	@FXML private Rectangle btn6;
+	@FXML private Rectangle btn7;
+	@FXML private Rectangle btn8;
+	@FXML private Rectangle btn9;
+	@FXML private Rectangle btn10;
 	@FXML private Rectangle btn11;
 	@FXML private Rectangle btn12;
 	@FXML private Rectangle btn13;
@@ -18,6 +29,8 @@ public class RootController implements Initializable {
 	@FXML private Rectangle btn16;
 	@FXML private Rectangle btn17;
 	@FXML private Rectangle btn18;
+	@FXML private Rectangle btn19;
+	@FXML private Rectangle btn20;
 	@FXML private Rectangle btn21;
 	@FXML private Rectangle btn22;
 	@FXML private Rectangle btn23;
@@ -26,6 +39,8 @@ public class RootController implements Initializable {
 	@FXML private Rectangle btn26;
 	@FXML private Rectangle btn27;
 	@FXML private Rectangle btn28;
+	@FXML private Rectangle btn29;
+	@FXML private Rectangle btn30;
 	@FXML private Rectangle btn31;
 	@FXML private Rectangle btn32;
 	@FXML private Rectangle btn33;
@@ -34,6 +49,8 @@ public class RootController implements Initializable {
 	@FXML private Rectangle btn36;
 	@FXML private Rectangle btn37;
 	@FXML private Rectangle btn38;
+	@FXML private Rectangle btn39;
+	@FXML private Rectangle btn40;
 	@FXML private Rectangle btn41;
 	@FXML private Rectangle btn42;
 	@FXML private Rectangle btn43;
@@ -42,6 +59,8 @@ public class RootController implements Initializable {
 	@FXML private Rectangle btn46;
 	@FXML private Rectangle btn47;
 	@FXML private Rectangle btn48;
+	@FXML private Rectangle btn49;
+	@FXML private Rectangle btn50;
 	@FXML private Rectangle btn51;
 	@FXML private Rectangle btn52;
 	@FXML private Rectangle btn53;
@@ -50,34 +69,84 @@ public class RootController implements Initializable {
 	@FXML private Rectangle btn56;
 	@FXML private Rectangle btn57;
 	@FXML private Rectangle btn58;
+	@FXML private Rectangle btn59;
+	@FXML private Rectangle btn60;
 	@FXML private Rectangle btn61;
 	@FXML private Rectangle btn62;
 	@FXML private Rectangle btn63;
 	@FXML private Rectangle btn64;
-	@FXML private Rectangle btn65;
-	@FXML private Rectangle btn66;
-	@FXML private Rectangle btn67;
-	@FXML private Rectangle btn68;
-	@FXML private Rectangle btn71;
-	@FXML private Rectangle btn72;
-	@FXML private Rectangle btn73;
-	@FXML private Rectangle btn74;
-	@FXML private Rectangle btn75;
-	@FXML private Rectangle btn76;
-	@FXML private Rectangle btn77;
-	@FXML private Rectangle btn78;
-	@FXML private Rectangle btn81;
-	@FXML private Rectangle btn82;
-	@FXML private Rectangle btn83;
-	@FXML private Rectangle btn84;
-	@FXML private Rectangle btn85;
-	@FXML private Rectangle btn86;
-	@FXML private Rectangle btn87;
-	@FXML private Rectangle btn88;
+	
+	public ArrayList<Rectangle> launchpad = new ArrayList<Rectangle>(); 
+	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		System.out.println("initialized!");
+		
+		launchpad.add(btn1);
+		launchpad.add(btn2);
+		launchpad.add(btn3);
+		launchpad.add(btn4);
+		launchpad.add(btn5);
+		launchpad.add(btn6);
+		launchpad.add(btn7);
+		launchpad.add(btn8);
+		launchpad.add(btn9);
+		launchpad.add(btn10);
+		launchpad.add(btn11);
+		launchpad.add(btn12);
+		launchpad.add(btn13);
+		launchpad.add(btn14);
+		launchpad.add(btn15);
+		launchpad.add(btn16);
+		launchpad.add(btn17);
+		launchpad.add(btn18);
+		launchpad.add(btn19);
+		launchpad.add(btn20);
+		launchpad.add(btn21);
+		launchpad.add(btn22);
+		launchpad.add(btn23);
+		launchpad.add(btn24);
+		launchpad.add(btn25);
+		launchpad.add(btn26);
+		launchpad.add(btn27);
+		launchpad.add(btn28);
+		launchpad.add(btn29);
+		launchpad.add(btn30);
+		launchpad.add(btn31);
+		launchpad.add(btn32);
+		launchpad.add(btn33);
+		launchpad.add(btn34);
+		launchpad.add(btn35);
+		launchpad.add(btn36);
+		launchpad.add(btn37);
+		launchpad.add(btn38);
+		launchpad.add(btn39);
+		launchpad.add(btn40);
+		launchpad.add(btn41);
+		launchpad.add(btn42);
+		launchpad.add(btn43);
+		launchpad.add(btn44);
+		launchpad.add(btn45);
+		launchpad.add(btn46);
+		launchpad.add(btn47);
+		launchpad.add(btn48);
+		launchpad.add(btn49);
+		launchpad.add(btn50);
+		launchpad.add(btn51);
+		launchpad.add(btn52);
+		launchpad.add(btn53);
+		launchpad.add(btn54);
+		launchpad.add(btn55);
+		launchpad.add(btn56);
+		launchpad.add(btn57);
+		launchpad.add(btn58);
+		launchpad.add(btn59);
+		launchpad.add(btn60);
+		launchpad.add(btn61);
+		launchpad.add(btn62);
+		launchpad.add(btn63);
+		launchpad.add(btn64);
 
 		try {
 			MusicPlayer mp = new MusicPlayer(this);
@@ -86,6 +155,7 @@ public class RootController implements Initializable {
 			ex.printStackTrace(); 
 		}
 		
+		// setOnActions Directly on the programming.
 //		btn11.setOnAction(new EventHandler<ActionEvent>() {
 //			@Override
 //			public void handle(ActionEvent event) {
@@ -97,8 +167,10 @@ public class RootController implements Initializable {
 //		btn13.setOnAction(event->handleBtn13Action(event));
 	}
 	
+	@FXML
 	public void handleBtn11Action() {
 		System.out.println("버튼11 클릭");
+		btn1.setFill(Color.RED);
 	}
 	
 	public void handleBtn12Action() {
